a7e18dd84d6ac1efa40975ab6ebda68235f1bd43
CarlosIdex <113919407+CarlosIdex@users.noreply.github.com>
2023-01-17 14:31:40 -0300

    Adicionar arquivos de projeto.

---
+++ AppMultiProjectAPI/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppMultiProjectAPI.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Entities/Entities/ClientConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class ClientConfiguration
+    {
+        public string BaseUrl { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+
+    }
+}
